2f05388c8c457559cb63b82d23404b6820dec89e
Fikri Ramadhan <fidhan76@gmail.com>
2024-03-11 13:07:58 +0700

    Add files via upload

---
+++ nomor1.java
@@ -0,0 +1,40 @@
+package tugasAlprog2;
+import java.util.Scanner;
+public class nomor1 {
+    public static boolean ValidasiUtama(String Sandi){
+        return Sandi.length() >= 8 && ValidasiAdaDigit(Sandi) && ValidasiAdaChar(Sandi) && ValidasiJumlahDigit(Sandi) >= 2;
+    }
+    public static boolean ValidasiAdaDigit(String str){
+        for (char c : str.toCharArray()){
+            if (Character.isDigit(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static boolean ValidasiAdaChar(String str){
+        for (char c : str.toCharArray()){
+            if (Character.isLetter(c)){
+                return true;
+            }
+        }
+        return false;
+    }
+    public static int ValidasiJumlahDigit(String str){
+        int hitung = 0;
+        for (char c : str.toCharArray()){
+            if (Character.isDigit(c)){
+                hitung++;
+            }
+        }
+        return hitung;
+    }
+    public static void main(String[] args) {
+        Scanner x = new Scanner(System.in);
+        System.out.print("Masukkan Sandi : ");
+        String Sandi = x.nextLine();
+
+        String hasil = ValidasiUtama(Sandi) ? "Kata sandi Valid" : "Kata Sandi Tidak Valid";
+        System.out.println(hasil);
+    }
+}
---
+++ nomor2.java
@@ -0,0 +1,30 @@
+package tugasAlprog2;
+import java.util.Scanner;
+public class nomor2 {
+    public static int numberOfDaysInAYear(int tahun){
+        if(tahun % 4 == 0){
+            int jumlah_hari = 366;
+            return jumlah_hari;
+        }
+        else {
+            int jumlah_hari = 365;
+            return jumlah_hari;
+        }
+    }
+    public static void main(String[] args) {
+        Scanner input1 = new Scanner(System.in);
+        System.out.print("Input tahun awal : ");
+        int tahun_awal = input1.nextInt();
+
+        Scanner input2 = new Scanner(System.in);
+        System.out.print("Input tahun akhir : ");
+        int tahun_akhir = input2.nextInt();
+        
+        int totalHari = 0;
+        for(int tahun = tahun_awal; tahun <= tahun_akhir; tahun++){
+            totalHari += numberOfDaysInAYear(tahun);
+        }
+        System.out.println("Jumlah hari dari tahun "+tahun_awal+" hingga tahun "+tahun_akhir+" adalah : "+totalHari); 
+        }
+    }
+
