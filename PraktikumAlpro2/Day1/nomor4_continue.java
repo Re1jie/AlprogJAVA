0b2e06567c615fc8bb2e45674acf2a56cfa482c2
Fikri Ramadhan <fidhan76@gmail.com>
2024-03-18 05:23:05 +0700

    Create nomor4_continue.java

---
+++ PraktikumAlpro2/Day1/nomor4_continue.java
@@ -0,0 +1,15 @@
+package PraktikumAlproDay1;
+
+public class TugasPraktikumNo4_2 {
+    //Contoh menggunakan continue
+    public static void main(String args[])
+    {
+        for (int i = 0; i < 10; i++) {
+            // Jika i = 2, maka Continue (atau di skip)
+            if (i == 2)
+                continue;
+ 
+            System.out.print(i + " ");
+        }
+    }
+}
