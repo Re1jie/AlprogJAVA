549b667e7efa382eb6db7f0f517b83c727deaedd
Fikri Ramadhan <fidhan76@gmail.com>
2024-03-18 05:22:34 +0700

    nomor4_break.java

---
+++ PraktikumAlpro2/Day1/nomor4_break.java
@@ -0,0 +1,15 @@
+package PraktikumAlproDay1;
+
+public class TugasPraktikumNo4_1 {
+    //Contoh kasus loop dengan break
+    public static void main(String[] args){
+        // Loop inisiasi i dari 0 hingga 100
+        for (int i = 0; i <= 100; i++) {
+            // Menghentikan loop disaat i = 5
+            if (i == 5)
+                break;
+            System.out.println("i: " + i);
+        }
+        System.out.println("Out of Loop");
+    }
+}
