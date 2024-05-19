be45a1eaf5bc5a949348ba83c64954417e9b85c5
Fikri Ramadhan <fidhan76@gmail.com>
2024-05-19 21:11:51 +0700

    Create Bentuk.java

---
+++ Alpro2OOP/Bentuk.java
@@ -0,0 +1,19 @@
+package Alpro2OOP;
+
+public class Bentuk {
+    private String warna;
+
+    public Bentuk(String warna){
+        this.warna=warna;
+        System.out.println("Konstruktor Bentuk triggered!");
+    }
+    public void serWarna(String warna){
+        this.warna=warna;
+    }
+    public String getWarna(){
+        return warna;
+    }
+    public void printInfo(){
+        System.out.println("Bentuk berwarna = "+warna);
+    }
+}
