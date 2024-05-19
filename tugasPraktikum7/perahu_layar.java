155e384e287e726de74b08795af308cdfbd8dc45
Fikri Ramadhan <fidhan76@gmail.com>
2024-05-19 21:14:32 +0700

    Create perahu_layar.java

---
+++ tugasPraktikum7/perahu_layar.java
@@ -0,0 +1,16 @@
+package tugasPraktikum7;
+
+public class perahu_layar extends kendaraan_laut{
+    private int jumlahlayar;
+
+    perahu_layar(String wn, String bb, int km, String jj, int jl) {
+        super(wn, bb, km, jj);
+        jumlahlayar = jl;
+    }
+
+    @Override
+    public void inpo() {
+        super.inpo();
+        System.out.println("Infokan jumlah layar = "+jumlahlayar);
+    }
+}
