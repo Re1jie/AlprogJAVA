709677609ff6c1305da27c51450c07cd7cd26bf5
Fikri <fidhan76@gmail.com>
2024-05-21 12:13:56 +0700

    materi MethodReturnObject

---
+++ kelas_21Mei2024/Rasional.java
@@ -0,0 +1,71 @@
+package kelas_21Mei2024;
+
+public class Rasional{
+    private int pemb;
+    private int peny;
+
+    public Rasional(){
+        peny=1;
+        pemb=0;
+    }
+
+    Rasional(int pemb, int peny){
+        this.pemb=pemb;
+        this.peny=peny;
+    }
+
+    public void SetPemb(int pemb){
+        this.pemb=pemb;
+    }
+
+    public int GetPemb(){
+        return pemb;
+    }
+
+    public void SetPeny(int peny){
+        this.peny=peny;
+    }
+    public int GetPeny(){
+        return peny;
+    }
+
+    public void info(){
+        System.out.println("Bilangan rasional dengan Pembilang = "+pemb);
+        System.out.println(" dan penyebut = "+peny);
+    }
+
+    public Rasional kaliKonstanta(int c){
+        Rasional hasil = new Rasional();
+
+        hasil.pemb = pemb*c;
+        hasil.peny = peny;
+        return hasil;
+    }
+    
+    public boolean IsSama(Rasional A){
+        boolean hasil;
+
+        hasil = A.GetPemb()*this.GetPeny()==A.GetPeny()*this.GetPemb();
+        return hasil;
+    }
+    
+    public Rasional Tambah(Rasional A){
+        Rasional hasil = new Rasional();
+
+        hasil.pemb = this.GetPemb()*A.GetPeny()+A.GetPemb()*this.GetPeny();
+        hasil.peny = this.GetPeny()*A.GetPeny();
+        return (hasil);
+    }
+
+    public double ToDesimal(){
+        return ((double)pemb/(double)peny);
+    }
+
+    public void IsEqual(Rasional A){
+        if(A.GetPemb()*this.GetPeny()==A.GetPeny()*this.GetPemb()){
+            System.out.println("Bilangan A sama dengan B ");
+        } else {
+            System.out.println("Bilangan A tidak sama B ");
+        }
+    }
+}
---
+++ kelas_21Mei2024/tesRasional.java
@@ -0,0 +1,28 @@
+package kelas_21Mei2024;
+
+public class tesRasional {
+    public static void main(String[] args) {
+        Rasional Bil1 = new Rasional(2, 4);
+        Rasional Bil2 = new Rasional(6, 12);
+        Bil1.info();
+
+        Rasional Bil3 = new Rasional();
+
+        Bil3 = Bil1.kaliKonstanta(10);
+        Bil3.info();
+
+        //ngecek bil sama atau nggak
+        if(Bil1.IsSama(Bil3)){
+            System.out.println("Sama");
+        } else {
+            System.out.println("Tidak Sama");
+        }
+
+        Rasional Bil4 = new Rasional();
+        Bil4 = Bil1.Tambah(Bil2);
+        Bil4.info();
+
+        System.out.println("Nilai Desimal : "+Bil1.ToDesimal());
+        Bil1.IsEqual(Bil2);
+    }
+}
