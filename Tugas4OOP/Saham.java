384aa21e51da5df9be2cd958b8a1b667f06320a0
Fikri <fidhan76@gmail.com>
2024-05-26 08:55:42 +0700

    create class 'Saham'

---
+++ Tugas4OOP/Saham.java
@@ -0,0 +1,49 @@
+package Tugas4OOP;
+
+public class Saham {
+    private String simbol;
+    private String nama;
+    private double priorClosingPrice;
+    private double currentPrice;
+    
+    public Saham(String simbol, String nama){
+        this.simbol=simbol;
+        this.nama=nama;
+    }
+    
+    public double getChangePercent(){
+        return ((currentPrice - priorClosingPrice) / priorClosingPrice) * 100;
+    }
+
+    public void setSimbol(String simbol){
+        this.simbol=simbol;
+    }
+
+    public String getSimbol(){
+        return simbol;
+    }
+    
+    public void setNama(String nama){
+        this.nama=nama;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+
+    public void setpriorClosingPrice(double priorClosingPrice){
+        this.priorClosingPrice=priorClosingPrice;
+    }
+
+    public double getpriorClosingPrice(){
+        return priorClosingPrice;
+    }
+
+    public void setcurrentPrice(double currentPrice){
+        this.currentPrice=currentPrice;
+    }
+
+    public double getcurrentPrice(){
+        return currentPrice;
+    }
+}
